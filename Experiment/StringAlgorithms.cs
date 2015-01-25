60bee674565fba0a0d41208ab58178d91b320057
ddelucie <ddelucie@hotmail.com>
2015-01-24 17:59:32 -0800

    added stringAgoriths

---
+++ Experiment/StringAlgorithms.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Experiment
+{
+	public static class StringAlgorithms
+	{
+		//determine if string has unique chars without using any data structure
+		public static bool HasUniqueChars(string s)
+		{
+			for (int i = 0; i < s.Length; i++)
+			{
+				char c = s.ElementAt(i);
+				string reducedString = s.Remove(i);
+				if (reducedString.Contains(c)) return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ ExperimentTests/StringAlgorithmsTests.cs
@@ -0,0 +1,35 @@
+﻿using Experiment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExperimentTests
+{
+	[TestClass]
+	public class StringAlgorithmsTests
+	{
+		[TestMethod]
+		public void TestHasUniqueChars()
+		{
+			string input;
+			bool result;
+
+			input = "nonunique";
+
+			result = StringAlgorithms.HasUniqueChars(input);
+
+			Assert.AreEqual(false, result);
+
+			input = "Unique";
+
+			result = StringAlgorithms.HasUniqueChars(input);
+
+			Assert.AreEqual(true, result);
+
+
+			input = "";
+
+			result = StringAlgorithms.HasUniqueChars(input);
+
+			Assert.AreEqual(true, result);
+		}
+	}
+}
