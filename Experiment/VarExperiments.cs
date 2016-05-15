151eea9dbd947a868d9a1b4bbc26fdbd1827b1ec
ddelucie <ddelucie@hotmail.com>
2016-05-15 15:43:39 -0700

    added var expereiments

---
+++ Experiment/VarExperiments.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment
+{
+
+
+	public class VarExperiments
+	{
+
+		public void DoThings()
+		{
+			BaseClass myBase1 = new InheritedClass();
+			string name = myBase1.Name();
+			var myBase2 = new InheritedClass();
+			name = myBase2.Name();
+
+			var customers = new List<BaseAbstractImpl>();
+			var distributors = new List<BaseAbstractImpl>();
+
+			var innerJoinQuery =
+				from cust in customers
+				join dist in distributors on cust.U equals dist.U
+				select new { CustomerName = cust.V, DistributorName = dist.V };
+
+
+		}
+	}
+}
